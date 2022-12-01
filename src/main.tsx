2ff1dfa4fe5096a0bf8279e97961fd954138b086
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-12-01 06:26:55 -0600

    amplify initialized

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
